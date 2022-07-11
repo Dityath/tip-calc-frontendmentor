07820ac5bebaf07f0c073e9868e9474defa677b9
dityath <dityaathallah107@gmail.com>
2022-07-11 16:31:54 +0700

    add js and some things

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,17 @@
 <script setup>
-import Screen from "./components/Screen.vue";
 import Title from "./components/Title.vue";
-import People from "./components/People.vue";
+import Result from "./components/Result.vue";
+import { ref } from "vue";
+
+const bill = ref(0);
+const tip = ref(5);
+const people = ref(1);
+
+const reset = () => {
+  bill.value = 0;
+  tip.value = 5;
+  people.value = 1;
+};
 </script>
 
 <template>
@@ -12,12 +22,33 @@ import People from "./components/People.vue";
     <div class="flex">
       <div class="flex-left">
         <Title title="Bill" />
-        <Screen />
+        <div class="screen">
+          <img src="./assets/icon-dollar.svg" alt="" />
+          <input v-model="bill" placeholder="Enter amount" />
+        </div>
         <Title title="Select Tip %" />
+        <div class="select-tip">
+          <button @click="tip = 5" :class="{ active: tip === 5 }">5%</button>
+          <button @click="tip = 10" :class="{ active: tip === 10 }">10%</button>
+          <button @click="tip = 15" :class="{ active: tip === 15 }">15%</button>
+          <button @click="tip = 25" :class="{ active: tip === 25 }">25%</button>
+          <button @click="tip = 50" :class="{ active: tip === 50 }">50%</button>
+        </div>
         <Title title="Number of People" />
-        <People />
+        <div class="screen">
+          <img src="./assets/icon-person.svg" alt="" />
+          <input type="text" v-model="people" placeholder="Enter people" />
+        </div>
       </div>
-      <div class="flex-right"></div>
+      <div class="flex-right">
+        <Result
+          :tip-amount="
+            people != 0 ? ((bill * tip) / 100 / people).toFixed(2) : 'Err'
+          "
+          :total="people != 0 ? (bill / people).toFixed(2) : 'Err'"
+          :onClick="reset"
+        />
+      </div>
     </div>
   </main>
 </template>
@@ -69,5 +100,65 @@ main {
   padding: 2rem;
   background-color: white;
   border-radius: 1.5rem 1.5rem 0 0;
+}
+
+.select-tip {
+  display: grid;
+  grid-template-columns: auto auto;
+  gap: 0.75rem;
+  margin-bottom: 1rem;
+}
+
+.select-tip button {
+  border: none;
+  background-color: var(--neutral-cyan);
+  font-size: 1.2rem;
+  font-weight: bold;
+  color: white;
+  font-family: var(--font);
+  cursor: pointer;
+  border-radius: 0.5rem;
+  padding: 0.75rem;
+  transition: all 0.2s ease-in-out;
+}
+
+.select-tip .active {
+  background-color: var(--primary);
+}
+
+.select-tip button:hover {
+  opacity: 0.8;
+}
+
+.screen {
+  padding: 10px 20px;
+  margin-bottom: 20px;
+  background-color: var(--neutral-light-cyan);
+  border-radius: 0.5rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.screen img {
+  width: 10px;
+  height: 16px;
+}
+
+.screen input {
+  margin: 0;
+  padding: 0;
+  font-size: 1.2rem;
+  font-weight: bold;
+  width: 100%;
+  text-align: right;
+  border: none;
+  background-color: transparent;
+  font-family: var(--font);
+  color: var(--neutral-cyan);
+}
+
+.screen input:focus {
+  outline: none;
 }
 </style>
---
+++ src/components/Result.vue
@@ -0,0 +1,84 @@
+<script setup>
+defineProps({
+  tipAmount: Number,
+  total: Number,
+  onClick: Function,
+});
+</script>
+
+<template>
+  <div class="tip-amount">
+    <div class="flex-inside">
+      <div class="flex-box-left">
+        <h3>Tip Amount</h3>
+        <p>/ person</p>
+      </div>
+      <div class="flex-box-right">
+        <h3>${{ tipAmount }}</h3>
+      </div>
+    </div>
+    <div class="flex-inside">
+      <div class="flex-box-left">
+        <h3>Total</h3>
+        <p>/ person</p>
+      </div>
+      <div class="flex-box-right">
+        <h3>${{ total }}</h3>
+      </div>
+    </div>
+    <button class="btn" @click="onClick">RESET</button>
+  </div>
+</template>
+
+<style scoped>
+.tip-amount {
+  background-color: var(--neutral-cyan);
+  font-size: 1.1rem;
+  padding: 1.5rem 1.4rem 1.4rem;
+  border-radius: 1rem;
+  gap: 1rem;
+  display: flex;
+  flex-direction: column;
+  color: white;
+}
+
+.flex-inside {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.flex-box-left h3 {
+  font-size: 1.1rem;
+}
+
+.flex-box-left p {
+  font-size: 0.9rem;
+  color: var(--neutral-gray-cyan);
+}
+
+.flex-box-right h3 {
+  font-size: 1.8rem;
+  color: var(--primary);
+  max-width: 150px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+
+.btn {
+  background-color: var(--primary);
+  border: none;
+  margin-top: 1rem;
+  border-radius: 0.5rem;
+  padding: 0.8rem 1rem;
+  font-size: 1rem;
+  color: var(--neutral-cyan);
+  font-weight: bold;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+}
+
+.btn:hover {
+  opacity: 0.8;
+}
+</style>
